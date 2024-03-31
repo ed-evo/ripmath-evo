8808467d7e8e5661ca0b24c8bb3859a3c875a80d
Lingyong Sun <lingyong.sun@gmail.com>
2024-03-31 12:01:35 +0200

    update app navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,10 @@
   <v-app>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
     >
     <LysNav />
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" app>
+    <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <!-- <v-toolbar-title v-text="title" /> -->
     </v-app-bar>
@@ -18,14 +14,12 @@
         <NuxtPage />
       </v-container>
     </v-main>
-    <v-footer app>
+    <v-footer>
       <span>&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
 
 <script setup>
-const clipped = ref(false)
 const drawer = ref(true)
-const miniVariant = ref(false)
 </script>
