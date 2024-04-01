0195ff08ab71b1e5a6033f5b9b224ecd0c5f48f0
Lingyong Sun <lingyong.sun@gmail.com>
2024-04-01 11:22:01 +0200

    tmp: aggiunta link alla pagina original

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,11 +6,12 @@
     <LysNav />
     </v-navigation-drawer>
     <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <!-- <v-toolbar-title v-text="title" /> -->
+      <v-app-bar-nav-icon :path="route.path" @click="drawer = !drawer" />
+      <a :href="`https://www.ripmat.it${ originalPath }`" target="_blank">{{ originalPath }}</a>
     </v-app-bar>
     <v-main>
       <v-container>
+        <pre>{{  data }}</pre>
         <NuxtPage />
       </v-container>
     </v-main>
@@ -21,5 +22,17 @@
 </template>
 
 <script setup>
+const route = useRoute()
 const drawer = ref(true)
+const originalPath = ref(null)
+
+watch(
+  () => route.path,
+  async newPath => {
+    const data = await queryContent(newPath).findOne()
+    originalPath.value = data.originalPath
+  },
+  { immediate: true }
+)
+
 </script>
