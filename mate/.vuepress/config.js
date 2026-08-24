967b4f66f5c03277f097faf9973bb6756babf383
Lingyong Sun <lingyong.sun@gmail.com>
2026-08-24 22:59:17 +0200

    refactor: init vuepress

---
+++ mate/.vuepress/config.js
@@ -0,0 +1,25 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { markdownMathPlugin } from '@vuepress/plugin-markdown-math'
+
+export default defineUserConfig({
+  lang: 'it-IT',
+
+  title: 'RipMat EVO',
+  description: 'Ripasso di Matematica',
+
+  theme: defaultTheme({
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+
+  plugins: [
+    markdownMathPlugin({
+      type: 'katex',
+
+    })
+  ],
+})
