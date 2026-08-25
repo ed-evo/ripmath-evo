5d79c340310c3de5dd6d5cf10eac9dac99434696
lysz210 <lysz210@gmail.com>
2026-08-25 19:26:28 +0000

    Deploying to gh-pages from @ ed-evo/ripmath-evo@5c273c88e3bb07df4691ecc7872bc74d20107c52 🚀

--- assets/404.html-Rxa19-js.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,b as a,o as n}from"./app-CZwbWa3X.js";const l={};function r(s,t){return n(),o("div",null,[...t[0]||(t[0]=[a("p",null,"404 Not Found",-1)])])}const i=e(l,[["render",r]]),_=JSON.parse('{"path":"/404.html","title":"","lang":"it-IT","frontmatter":{"layout":"NotFound"},"headers":[],"git":{},"filePathRelative":null}');export{i as comp,_ as data};
